86fbc4bdfc2b45ea02ae1ef8efbb8eaac09694dc
htung1901 <nvht2004@gmail.com>
2025-02-26 20:00:03 +0700

    Home student #1

--- src/app/pages/home/student-home/student-home.component.ts
+++ src/app/pages/home/student-home/student-home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../layout/header/header.component";
 
 @Component({
   selector: 'app-student-home',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './student-home.component.html',
   styleUrl: './student-home.component.scss'
 })
 export class StudentHomeComponent {
-
 }
