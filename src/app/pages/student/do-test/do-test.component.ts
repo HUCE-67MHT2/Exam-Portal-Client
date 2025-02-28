9160be9eda2b10dbfe02aa088d7c2b0579a35e91
longt <longthan243@gmail.com>
2025-02-28 10:43:24 +0700

    refactor: update student exam route to use new ExamComponent

--- src/app/pages/student/do-test/do-test.component.ts
+++ src/app/pages/student/do-test/do-test.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-do-test',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './do-test.component.html',
   styleUrl: './do-test.component.scss'
 })
 export class DoTestComponent {
+  subject = "Toán-GK";
 
+  // Mảng lưu đáp án của sinh viên (giá trị mặc định là rỗng)
+  answers: string[][] = Array(5).fill(null).map(() => Array(6).fill(""));
+
+  options = ['A', 'B', 'C', 'D']; // Các đáp án có thể chọn
+
+  submit() {
+    console.log("Bài làm đã nộp!", this.answers);
+  }
 }
