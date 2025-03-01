1485fec2a6018d54b6943418ed59a20c301cc010
htung1901 <nvht2004@gmail.com>
2025-03-01 09:45:18 +0700

    Exam student #1

--- src/app/pages/student/exam/exam.component.ts
+++ src/app/pages/student/exam/exam.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../layout/header/header.component";
+import { SearchBarComponent } from "../../../layout/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-exam',
-  imports: [],
+  imports: [HeaderComponent, SearchBarComponent],
   templateUrl: './exam.component.html',
   styleUrl: './exam.component.scss'
 })
