2afb4496e69dbc6772500c77958808d0d14adbdf
htung1901 <nvht2004@gmail.com>
2025-03-18 20:26:13 +0700

    Dropdown user selection

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,21 @@
-import {Component} from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
   standalone: true,
 })
 export class HeaderComponent {
+  isUserInfoSelectionVisible = false;
+
+  toggleUserInfoSelection(event: Event) {
+    event.stopPropagation();
+    this.isUserInfoSelectionVisible = !this.isUserInfoSelectionVisible;
+  }
+
+  @HostListener("document:click", ["$event"])
+  onDocumentClick(event: Event) {
+    this.isUserInfoSelectionVisible = false;
+  }
 }
