61b2e7be3a030de183842d824adf9ac3426bf5cb
htung1901 <nvht2004@gmail.com>
2025-03-25 22:16:41 +0700

    feat: enhance student list page with table layout and search bar integration

--- src/app/pages/teacher/exam_session/exam-session-dashboard/student-list/student-list.component.ts
+++ src/app/pages/teacher/exam_session/exam-session-dashboard/student-list/student-list.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from "../../../../../layout/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-student-list',
-  imports: [],
+  imports: [SearchBarComponent],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.scss'
 })
