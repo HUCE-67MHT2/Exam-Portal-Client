dc073c6987d8bdc5463bc26a7f44f0fd2d7cbfeb
htung1901 <nvht2004@gmail.com>
2025-03-26 02:22:28 +0700

    feat: implement auto-generate exam component with tabbed interface for questions and exam info

--- src/app/pages/teacher/exam/auto-generate/create-auto-generate/create-auto-generate.component.ts
+++ src/app/pages/teacher/exam/auto-generate/create-auto-generate/create-auto-generate.component.ts
@@ -1,14 +1,19 @@
 import { Component } from "@angular/core";
-import { HeaderComponent } from "../../../../../layout/header/header.component";
-import { HeaderStudentComponent } from "../../../../../layout/header/header-student/header-student.component";
 import { HeaderBackComponent } from "../../../../../layout/header-back/header-back.component";
+import { QuestionComponent } from "./question/question.component";
+import { InfoComponent } from "./info/info.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-create-auto-generate",
-  imports: [HeaderComponent, HeaderStudentComponent, HeaderBackComponent],
+  imports: [HeaderBackComponent, QuestionComponent, CommonModule, InfoComponent],
   templateUrl: "./create-auto-generate.component.html",
   styleUrl: "./create-auto-generate.component.scss",
 })
 export class CreateAutoGenerateComponent {
+  activeTab: string = "left";
 
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
