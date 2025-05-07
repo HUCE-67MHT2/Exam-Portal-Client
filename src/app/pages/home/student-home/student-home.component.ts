93852f7bfe62d61513561b40e41a2bd8f1ec6f03
htung1901 <nvht2004@gmail.com>
2025-05-07 08:52:36 +0700

    feat: enhance student home component with improved achievement view handling and layout adjustments

--- src/app/pages/home/student-home/student-home.component.ts
+++ src/app/pages/home/student-home/student-home.component.ts
@@ -1,17 +1,24 @@
 import {Component} from '@angular/core';
 import {HeaderStudentComponent} from '../../../layout/header/header-student/header-student.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-student-home',
-  imports: [HeaderStudentComponent],
+  imports: [HeaderStudentComponent, CommonModule],
   templateUrl: './student-home.component.html',
   styleUrl: './student-home.component.scss'
 })
 export class StudentHomeComponent {
   activeSection: string = 'schedule';
+  achievementView: string = 'general'; // Default view
 
   scrollToSection(elementId: string): void {
     this.activeSection = elementId;
     document.getElementById(elementId)?.scrollIntoView({behavior: 'smooth'});
+  }
+
+  switchAchievementView(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    this.achievementView = select.value;
   }
 }
